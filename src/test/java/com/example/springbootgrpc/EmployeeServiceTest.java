062121cd48475a56acf1cb54e407a4de43dd589d
chethani76 <chethaniwijesekara@gmail.com>
2020-10-31 18:29:05 +0530

    grpc request test

--- src/test/java/com/example/springbootgrpc/EmployeeServiceTest.java
+++ src/test/java/com/example/springbootgrpc/EmployeeServiceTest.java
@@ -1,9 +1,22 @@
 package com.example.springbootgrpc;
 
+import com.example.springbootgrpc.proto.Employee;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringBootTest
 @SpringJUnitConfig(classes = {EmployeeServiceTestConfiguration.class})
 public class EmployeeServiceTest {
+
+    @Autowired
+    private EmployeeServiceImpl employeeService;
+
+    @Test
+    void testemployee(){
+        Employee.EmpRequest request = Employee.EmpRequest.newBuilder()
+                .setEmail("test@wzy")
+                .build();
+    }
 }
