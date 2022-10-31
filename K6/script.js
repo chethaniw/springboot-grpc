b0876427a60515f4d5aeea5acbde371525792b7c
chethaniw <chethaniwijesekara@gmail.com>
2022-10-31 22:16:58 +0530

    load testing on basic api request

--- K6/script.js
+++ K6/script.js
@@ -3,5 +3,8 @@ import encoding from "k6/encoding";
 import http from "k6/http";
 
 export default function(){
-    let res = http.get("http://127.")
-}
+    let res = http.get("http://localhost:8080/api/v1/employee")
+    check(res,{
+        "is status 200": (r) => r.status ===200
+    });
+};
